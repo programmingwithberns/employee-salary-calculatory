47463bd92ba8f8d09aa9cf88cacd3dcd0c9e286b
Longho Bernard <blongho02@gmail.com>
2024-05-14 01:37:39 +0200

    Handling exception

--- employee_salary.py
+++ employee_salary.py
@@ -19,21 +19,24 @@ bonus_per_extra_hour = 100
 
 hours_worked = 0
 
+# getting input from the user
 user_input = input("Please enter the hours worked: ")
 
-hours_worked = float(user_input)
+try:
+    hours_worked = float(user_input)
+    weekly_salary = hourly_wage * hours_worked
 
-weekly_salary = hourly_wage * hours_worked
-
-if hours_worked < min_working_hours or hours_worked > max_working_hours:
+    if hours_worked < min_working_hours or hours_worked > max_working_hours:
         print(f"Weekly hours must be between {min_working_hours} and {max_working_hours}.")
-else:
-    if hours_worked <= normal_work_hours:
-        print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
-        print(f"You did not have any bonus this week.")
-    if hours_worked > normal_work_hours:
-        bonus_hours = hours_worked - normal_work_hours
-        bonus_wage = bonus_hours * bonus_per_extra_hour
-        gross_salay_with_bonus = weekly_salary + bonus_wage
-        print(f"Your weekly salary for {hours_worked} hours is {gross_salay_with_bonus} FCFA.")
-        print(f"You had a bonus of {bonus_wage} FCFA for the {bonus_hours} worked.")
+    else:
+        if hours_worked <= normal_work_hours:
+            print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
+            print(f"You did not have any bonus this week.")
+        if hours_worked > normal_work_hours:
+            bonus_hours = hours_worked - normal_work_hours
+            bonus_wage = bonus_hours * bonus_per_extra_hour
+            gross_salay_with_bonus = weekly_salary + bonus_wage
+            print(f"Your weekly salary for {hours_worked} hours is {gross_salay_with_bonus} FCFA.")
+            print(f"You had a bonus of {bonus_wage} FCFA for the {bonus_hours} worked.")
+except Exception as ve:
+    print(f"ERROR occured [{ve}]")
