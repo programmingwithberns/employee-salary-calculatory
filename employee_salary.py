5de91650b30a78301234ddaeea8e446287442330
Longho Bernard <blongho02@gmail.com>
2024-05-14 01:31:14 +0200

    Handling hours less than 40

--- employee_salary.py
+++ employee_salary.py
@@ -13,22 +13,25 @@ normal_work_hours = 40
 
 max_working_hours = 50
 
-min_working_hours = 1 
+min_working_hours = 1
 
 bonus_per_extra_hour = 100
 
-hours_worked = 0 
+hours_worked = 0
 
 user_input = input("Please enter the hours worked: ")
 
 hours_worked = float(user_input)
 
-weekly_salary = hourly_wage * hours_worked 
+weekly_salary = hourly_wage * hours_worked
 
-print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
 
-if hours_worked > normal_work_hours: 
-    bonus_hours =  hours_worked - normal_work_hours
+if hours_worked <= 40:
+    print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
+    print(f"You did not have any bonus this week.")
+if hours_worked > 40:
+    bonus_hours = hours_worked - normal_work_hours
     bonus_wage = bonus_hours * bonus_per_extra_hour
     gross_salay_with_bonus = weekly_salary + bonus_wage
+    print(f"Your weekly salary for {hours_worked} hours is {gross_salay_with_bonus} FCFA.")
     print(f"You had a bonus of {bonus_wage} FCFA for the {bonus_hours} worked.")
